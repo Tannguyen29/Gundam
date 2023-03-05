417cd377ab039635a3d63538b695c2dff2b9252d
Se-pher <hunglqgch210012@fpt.edu.vn>
2023-03-05 16:59:18 +0700

    Xong register

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var express = require('express');
 const { Types } = require('mongoose');
 var expressHbs = require('express-handlebars');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const router = express.Router();
 var app = express()
 
 
-app.set('view engine','hbs')
-app.use(express.urlencoded({extended:true}))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 
 var url = 'mongodb+srv://Gundam:Tanghj23@gundam.uglqxca.mongodb.net/test';
 var MongoClient = require('mongodb').MongoClient;
 
-app.post('/edit',async (req,res)=>{
+//PRODUCT IN ADMIN
+app.post('/edit', async (req, res) => {
     const Name = req.body.txtName
     const Price = req.body.txtPrice
     const Image = req.body.Image
@@ -22,64 +25,64 @@ app.post('/edit',async (req,res)=>{
     let client = await MongoClient.connect(url)
     let dbo = client.db("Gundam_store")
     var ObjectId = require('mongodb').ObjectId
-    const condition = {"_id" : new ObjectId(id)};
-    const newValues = {$set : {Name:Name,Price:Price,Image:Image,Quantity:Quantity,Type:Type}}
-    await dbo.collection("product").updateOne(condition,newValues)
-    res.redirect('/')
+    const condition = { "_id": new ObjectId(id) };
+    const newValues = { $set: { Name: Name, Price: Price, Image: Image, Quantity: Quantity, Type: Type } }
+    await dbo.collection("product").updateOne(condition, newValues)
+    res.redirect('/admin')
 })
 
-app.get('/edit/:id', async(req,res)=>{
+app.get('/edit/:id', async (req, res) => {
     const id = req.params.id
     let client = await MongoClient.connect(url)
     let dbo = client.db("Gundam_store")
     var ObjectId = require('mongodb').ObjectId
-    let condition = {"_id" : new ObjectId(id)};
+    let condition = { "_id": new ObjectId(id) };
     const prod = await dbo.collection("product").findOne(condition)
-    res.render('admin/edit',{prod:prod})
+    res.render('admin/edit', { prod: prod })
 })
 
-app.post('/add',async (req,res)=>{
+app.post('/add', async (req, res) => {
     const Name = req.body.txtName
     const Price = req.body.txtPrice
     const Image = req.body.Image
     const Quantity = req.body.Quantity
     const Type = req.body.Type
     //kiem tra input
-    if(Name.length <=5){
-        res.render('admin/add',{Name_err: 'Min length is 5 characters'})
+    if (Name.length <= 5) {
+        res.render('admin/add', { Name_err: 'Min length is 5 characters' })
         return
     }
     //
     const newProduct = {
         'Name': Name,
-        'Price':Price,
-        'Image':Image,
-        'Quantity':Quantity,
-        'Type':Type
+        'Price': Price,
+        'Image': Image,
+        'Quantity': Quantity,
+        'Type': Type
     }
     let client = await MongoClient.connect(url)
     let dbo = client.db("Gundam_store")
     await dbo.collection("product").insertOne(newProduct)
-    res.redirect("/")
+    res.redirect("/admin")
 
 })
 
-app.get('/add',(req,res)=>{
+app.get('/add', (req, res) => {
     res.render('admin/add')
 })
-app.get('/delete/:id',async (req,res)=>{
+app.get('/delete/:id', async (req, res) => {
     const id = req.params.id
     let client = await MongoClient.connect(url)
     let dbo = client.db("Gundam_store")
     var ObjectId = require('mongodb').ObjectId
-    let condition = {"_id" : new ObjectId(id)};
+    let condition = { "_id": new ObjectId(id) };
     await dbo.collection("product").deleteOne(condition)
-    res.redirect("/")
+    res.redirect("/admin")
 })
 
 
 // LOGIN
-app.get('/login', function(req, res) {
+app.get('/', function (req, res) {
     res.render('login/login_form');
 });
 
@@ -91,22 +94,43 @@ app.post('/login', (req, res) => {
 
 
 //REGISTER
-app.get('/register', function(req, res) {
+app.get('/register', function (req, res) {
     res.render('login/register_form');
+});
+
+app.post('/register', async (req, res) => {
+    const name = req.body.reg_name;
+    const email = req.body.reg_email;
+    const phone = req.body.reg_phone;
+    const password = req.body.reg_pass;
+
+    // Create New Document
+    const newUser = {
+        name,
+        email,
+        phone,
+        password
+    };
+
+    // Insert the new user document into the "users" collection
+    const db = client.db("Gundam_store");
+    await db.collection("users").insertOne(newUser);
+    res.redirect("/");
+
 });
 
 
 //ADMIN
-app.get('/admin',async (req,res)=>{
+app.get('/admin', async (req, res) => {
     let client = await MongoClient.connect(url)
     let dbo = client.db("Gundam_store")
     let products = await dbo.collection("product").find().toArray()
-    res.render('admin/main',{'product':products})
+    res.render('admin/main', { 'product': products })
 })
 
 
 //CONNECT 
 const PORT = process.env.PORT || 3000
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log("Server is up!")
 })
