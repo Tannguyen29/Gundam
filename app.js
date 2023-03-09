38bab0c470a9ddbe68020bcc48667c0d7246d7db
Se-pher <hunglqgch210012@fpt.edu.vn>
2023-03-09 09:22:04 +0700

    Validation

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var url = 'mongodb+srv://Gundam:Tanghj23@gundam.uglqxca.mongodb.net/test';
 var MongoClient = require('mongodb').MongoClient;
 
 // LOGIN
-app.get('/adminlogin', function (req, res) {
+app.get('/', function (req, res) {
     res.render('login/login_form');
 });
 
@@ -37,7 +37,7 @@ app.post('/login', async (req, res) => {
     if (!isPasswordValid) {
         return res.status(401).send('Invalid password');
     }
-    res.redirect("/user");
+    res.redirect("/mainpage");
 
 });
 
@@ -48,12 +48,49 @@ app.get('/register', function (req, res) {
 });
 
 app.post('/register', async (req, res) => {
-    
+
     const { name, email, phone, password, password2 } = req.body;
-    
+    // Validate input
+    let errors = [];
+
+    // Check required fields
+    if (!name || !email || !phone || !password || !password2) {
+        errors.push({ msg: 'Please fill in all fields' });
+    }
+
+    // Check name length
+    if (name.length < 2 || name.length > 30) {
+        errors.push({ msg: 'Name must be between 2 and 30 characters' });
+    }
+
+    // Check phone format
+    const phoneRegex = /^\d{10}$/;
+    if (!phoneRegex.test(phone)) {
+        errors.push({ msg: 'Phone must be 10 digits' });
+    }
+
+    // Check password length
+    if (password.length < 6 || password.length > 20) {
+        errors.push({ msg: 'Password must be between 6 and 20 characters' });
+    }
+
+    // Check passwords match
+    if (password !== password2) {
+        errors.push({ msg: 'Passwords do not match' });
+    }
+
+    if (errors.length > 0) {
+        res.render('login/register_form', {
+            errors,
+            name,
+            email,
+            phone,
+            password,
+            password2
+        });
+    } else {
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
-
         // Create new document
         const newUser = {
             name,
@@ -61,13 +98,16 @@ app.post('/register', async (req, res) => {
             phone,
             password: hashedPassword
         };
-
-        // Insert the new user document into the users collection
         let client = await MongoClient.connect(url)
         const db = client.db("Gundam_store");
         await db.collection("users").insertOne(newUser);
         res.redirect("/");
-    
+    }
+
+
+    // Insert the new user document into the users collection
+
+
 });
 
 //Product Detail
@@ -84,14 +124,14 @@ app.get('/detail/:id', async (req, res) => {
 
 //add to cart
 app.post('/detail', async (req, res) => {
-    const { id, quantity} = req.body;
+    const { id, quantity } = req.body;
     let client = await MongoClient.connect(url)
     let dbo = client.db("Gundam_store")
     var ObjectId = require('mongodb').ObjectId
     let condition = { "_id": new ObjectId(id) };
     const prod = await dbo.collection("product").findOne(condition)
 
-    if(prod.Quantity < quantity){
+    if (prod.Quantity < quantity) {
         res.render('user/detail', { prod: prod, err: "Quantity is not enough" })
         return
     }
@@ -102,14 +142,14 @@ app.post('/detail', async (req, res) => {
 })
 
 app.get('/checkout', async (req, res) => {
-    const { id, quantity} = req.query;
+    const { id, quantity } = req.query;
     let client = await MongoClient.connect(url)
     let dbo = client.db("Gundam_store")
     var ObjectId = require('mongodb').ObjectId
     let condition = { "_id": new ObjectId(id) };
     const prod = await dbo.collection("product").findOne(condition)
 
-    if(prod.Quantity < quantity){
+    if (prod.Quantity < quantity) {
         res.render('user/detail', { prod: prod, err: "Quantity is not enough" })
         return
     }
@@ -120,13 +160,13 @@ app.get('/checkout', async (req, res) => {
 })
 
 app.post('/checkout', async (req, res) => {
-    const { id, quantity} = req.body;
+    const { id, quantity } = req.body;
     let client = await MongoClient.connect(url)
     let dbo = client.db("Gundam_store")
     var ObjectId = require('mongodb').ObjectId
     let condition = { "_id": new ObjectId(id) };
     const prod = await dbo.collection("product").findOne(condition)
-    const newQuantity = Number(prod.Quantity)- quantity
+    const newQuantity = Number(prod.Quantity) - quantity
     const newValues = { $set: { Name: prod.Name, Price: prod.Price, Image: prod.Image, Quantity: newQuantity, Type: prod.Type } }
     await dbo.collection("product").updateOne(condition, newValues)
 
@@ -135,13 +175,40 @@ app.post('/checkout', async (req, res) => {
 
 
 //ADMIN
+//Get Form admin
+app.get('/adminlogin', function (req, res) {
+    res.render('admin/adminlogin');
+});
+//Admin Login process
+app.post('/admin_login', async (req, res) => {
+    const { email, password } = req.body;
+
+    let client = await MongoClient.connect(url)
+    const db = client.db("Gundam_store");
+
+    const user = await db.collection('admin').findOne({ email });
+
+    if (!user) {
+
+        return res.status(401).send('Invalid email');
+    }
+
+
+    if (!password) {
+        return res.status(401).send('Invalid password');
+    }
+    res.redirect("/admin");
+
+});
+
+//Get main form admin
 app.get('/admin', async (req, res) => {
     let client = await MongoClient.connect(url)
     let dbo = client.db("Gundam_store")
     let products = await dbo.collection("product").find().toArray()
     res.render('admin/main', { 'product': products })
 })
-
+//edit function
 app.post('/edit', async (req, res) => {
     const Name = req.body.txtName
     const Price = req.body.txtPrice
@@ -158,7 +225,7 @@ app.post('/edit', async (req, res) => {
     await dbo.collection("product").updateOne(condition, newValues)
     res.redirect('/admin')
 })
-
+//get form edit
 app.get('/edit/:id', async (req, res) => {
     const id = req.params.id
     let client = await MongoClient.connect(url)
@@ -168,7 +235,7 @@ app.get('/edit/:id', async (req, res) => {
     const prod = await dbo.collection("product").findOne(condition)
     res.render('admin/edit', { prod: prod })
 })
-
+//add function
 app.post('/add', async (req, res) => {
     const Name = req.body.txtName
     const Price = req.body.txtPrice
@@ -194,7 +261,7 @@ app.post('/add', async (req, res) => {
     res.redirect("/admin")
 
 })
-
+//get form add
 app.get('/add', (req, res) => {
     res.render('admin/add')
 })
@@ -215,7 +282,7 @@ app.get('/user', async (req, res) => {
     let products = await dbo.collection("product").find().toArray()
     res.render('user/index', { 'product': products })
 })
-app.get('/', async (req, res) => {
+app.get('/mainpage', async (req, res) => {
     let client = await MongoClient.connect(url)
     let dbo = client.db("Gundam_store")
     let products = await dbo.collection("product").find().toArray()
